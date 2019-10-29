7e517aa85eecf7d8541610f01d23e8ecf4d683d1
Fahad AlMutairi <fahadalmutairi@MacBook-Pro-alkhas-b-Fahad.local>
2019-10-29 17:16:47 +0300

    a

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,5 @@
 from django.urls import path
-from .views import (
-    UserCreateAPIView,
-)
-from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
-    path('Singup/', UserCreateAPIView.as_view(), name='user-singup'),   
-    path('Login/', obtain_jwt_token, name='user-login'),
+    
 ]
